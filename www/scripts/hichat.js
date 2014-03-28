b04d483ab4c8fe00e0d795f8b5f9c8c636557e5b
Wayou <liuwayong@gmail.com>
2014-03-28 18:54:42 +0800

    emoji support

--- www/scripts/hichat.js
+++ www/scripts/hichat.js
@@ -1,5 +1,5 @@
 /*
- *hichat v0.2.0
+ *hichat v0.4.0
  *Wayou Mar 28,2014
  *view on GitHub:https://github.com/wayou/HiChat
  *see it in action:http://hichat.herokuapp.com/
@@ -105,23 +105,73 @@ HiChat.prototype = {
                 reader.readAsDataURL(file);
             };
         }, false);
+        this._initialEmoji();
+        document.getElementById('emoji').addEventListener('click', function(e) {
+            var emojiwrapper = document.getElementById('emojiWrapper');
+            emojiwrapper.style.display = 'block';
+            e.stopPropagation();
+        }, false);
+        document.body.addEventListener('click', function(e) {
+            var emojiwrapper = document.getElementById('emojiWrapper');
+            if (e.target != emojiwrapper) {
+                emojiwrapper.style.display = 'none';
+            };
+        });
+        document.getElementById('emojiWrapper').addEventListener('click', function(e) {
+            var target = e.target;
+            if (target.nodeName.toLowerCase() == 'img') {
+                var messageInput = document.getElementById('messageInput');
+                messageInput.focus();
+                messageInput.value = messageInput.value + '[emoji:' + target.title + ']';
+            };
+        }, false);
+    },
+    _initialEmoji: function() {
+        var emojiContainer = document.getElementById('emojiWrapper'),
+            docFragment = document.createDocumentFragment();
+        for (var i = 69; i > 0; i--) {
+            var emojiItem = document.createElement('img');
+            emojiItem.src = '../content/emoji/' + i + '.gif';
+            emojiItem.title = i;
+            docFragment.appendChild(emojiItem);
+        };
+        emojiContainer.appendChild(docFragment);
     },
     _displayNewMsg: function(user, msg, color) {
         var container = document.getElementById('historyMsg'),
-            msgToDisplay = document.createElement('p');
+            msgToDisplay = document.createElement('p'),
+            date = new Date().toTimeString().substr(0, 8),
+            //determine whether the msg contains emoji
+            msg = this._showEmoji(msg);
         msgToDisplay.style.color = color || '#000';
-        msgToDisplay.textContent = user + ': ' + msg;
+        msgToDisplay.innerHTML = user + '<span class="timespan">(' + date + '): </span>' + msg;
         container.appendChild(msgToDisplay);
         container.scrollTop = container.scrollHeight;
     },
     _displayImage: function(user, imgData, color) {
         var container = document.getElementById('historyMsg'),
             msgToDisplay = document.createElement('p'),
-            imgToDisplay = document.createElement('img');
+            date = new Date().toTimeString().substr(0, 8);
+        imgToDisplay = document.createElement('img');
         msgToDisplay.style.color = color || '#000';
-        msgToDisplay.innerHTML = user + ': <br/>' + '<a href="' + imgData + '" target="_blank"><img src="' + imgData + '"/></a>';
+        msgToDisplay.innerHTML = user + '<span class="timespan">(' + date + '): </span> <br/>' + '<a href="' + imgData + '" target="_blank"><img src="' + imgData + '"/></a>';
         imgToDisplay.src = imgData;
         container.appendChild(msgToDisplay);
         container.scrollTop = container.scrollHeight;
+    },
+    _showEmoji: function(msg) {
+        var match, result = msg,
+            reg = /\[emoji:\d+\]/g,
+            emojiIndex,
+            totalEmojiNum = document.getElementById('emojiWrapper').children.length;
+        while (match = reg.exec(msg)) {
+            emojiIndex = match[0].slice(7, -1);
+            if (emojiIndex > totalEmojiNum) {
+                result = result.replace(match[0], '[X]');
+            } else {
+                result = result.replace(match[0], '<img class="emoji" src="../content/emoji/' + emojiIndex + '.gif" />');
+            };
+        };
+        return result;
     }
 };
