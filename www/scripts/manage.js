df0978a2a77f6347db6994e79035a3d24bfad4cf
PhosphorusP <1309396166@qq.com>
2017-11-03 17:19:42 +0800

    Deleted some comments.

--- www/scripts/manage.js
+++ www/scripts/manage.js
@@ -1,10 +1,3 @@
-/*
- *hichat v0.4.2
- *Wayou Mar 28,2014
- *MIT license
- *view on GitHub:https://github.com/wayou/HiChat
- *see it in action:http://hichat.herokuapp.com/
- */
 var nickName
 window.onload = function() {
     var hichat = new HiChat();
