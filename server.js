c7be614f41e7d059113033992f64c2206b78fd57
ITAC85v2 <47468644+ITAC85v2@users.noreply.github.com>
2022-12-09 14:01:16 -0500

    Add debug messages, remove unnecessary comments

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express'),
     server = require('http').createServer(app),
     { Server } = require('socket.io');
     users = [];
-//specify the html we will use
 var io = new Server(server, {
   path: "/socket.io/",
   pingTimeout: 60000,
@@ -23,14 +22,15 @@ function fixss(input) {
 app.use('/', express.static(__dirname + '/www'));
 //bind the server to the 80 port
 //server.listen(3000);//for local test
-server.listen(process.env.PORT || 3000);//publish to heroku
-//server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);//publish to openshift
-//console.log('server started on port'+process.env.PORT || 3000);
+var listenport = process.env.PORT || 3000;
+server.listen(listenport);
+console.log('Server started on port'+listenport+'\nGo to http://127.0.0.1:'+listenport+'/ to view locally\n');
 //handle the socket
 io.sockets.on('connection', function(socket) {
     //new user login
     socket.on('login', function(nickname) {
-        if (users.indexOf(nickname) > -1) {
+        if (users.indexOf(nickname) > -1 && nickname.toLowerCase() != 'system') {
+            console.log('A user tried to join with existing nickname '+fixss(nickname));
             socket.emit('nickExisted');
         } else {
             //socket.userIndex = users.length;
@@ -39,22 +39,26 @@ io.sockets.on('connection', function(socket) {
             users.push(nick);
             socket.emit('loginSuccess');
             io.sockets.emit('system', nick, users.length, 'login');
+            console.log('User '+nick+' joined');
         };
     });
     //user leaves
     socket.on('disconnect', function() {
         if (socket.nickname != null) {
             //users.splice(socket.userIndex, 1);
+            console.log('user '+socket.nickname+' disconnected');
             users.splice(users.indexOf(socket.nickname), 1);
             socket.broadcast.emit('system', socket.nickname, users.length, 'logout');
         }
     });
     //new message get
     socket.on('postMsg', function(msg, color) {
+        console.log('got message from '+socket.nickname+' with content: '+fixss(msg));
         socket.broadcast.emit('newMsg', socket.nickname, fixss(msg), fixss(color));
     });
     //new image get
     socket.on('img', function(imgData, color) {
+        console.log('got image from '+socket.nickname+' with data size of '+imgData.length+' bytes');
         socket.broadcast.emit('newImg', socket.nickname, fixss(imgData), fixss(color));
     });
 });
